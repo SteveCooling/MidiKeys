121f3a1af081502b7ddf92f2132a973f0c977fbc
Morten <morten@bzzt.no>
2020-04-17 02:25:38 +0200

    Way less note hangs. Speed optimizations.

--- Arduino/MidiKeys/MidiKeys.ino
+++ Arduino/MidiKeys/MidiKeys.ino
@@ -10,6 +10,8 @@
 #define NUM_BANKS 16
 #define NUM_NOTES 128
 
+//#define SERIAL_DEBUG
+
 // When this matches a primary key switch has been trigged
 #define PRIMARY_BANK_MASK 0x01
 #define PRIMARY_BANK_BITS 0x01
@@ -17,12 +19,12 @@
 #include <math.h>
 #include "KeyboardMux.h"
 #include "MIDIUSB.h"
+#include "velocity.h"
 
 #define BLINKLED LED_BUILTIN
 #define MIDI_CHAN 0
 
 KeyboardMux keyboard;
-bool inverse_logic;
 bool primary_bank;
 
 // Timestamps for half events
@@ -31,41 +33,37 @@ unsigned long half_off[NUM_NOTES];
 // Stores computed velocity for each note
 unsigned char note_velocity[NUM_NOTES];
 
-char transpose = 56;
+char transpose = 24;
 
 float velocity_factor = 0.2;
 float velocity_curve  = 60;
 
+unsigned long time = micros();
+
 unsigned char bankAndPosToNote(int bank, unsigned char pos) {
   // XXX: This requires primary/second bank bit to be LSB
   unsigned char note = (bank>>1) * 8;
-  return note + pos + transpose;
+  return note + (7-pos) + transpose;
 }
 
 unsigned char microsToVelocity(unsigned long micros) {
-    float millis = micros/1000;
-    float velocity = velocity_curve * log(velocity_factor*millis);
-    if(velocity < 0) return 127;
-    if(velocity > 127) return 1;
-    return 127-(char)round(velocity);
-
+    if(micros >= 65535) micros = 65535;
+    // Drop the 8 lowest bits of the microsecond duration and lookup the corresponding MIDI velocity
+    unsigned char time = micros>>8;
+    return velocity_log[time];
 }
 
 void noteOn(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOn);
-  MidiUSB.flush();
 }
 
 void noteOff(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = {0x08, 0x80 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOn);
-  MidiUSB.flush();
 }
 
 void noteChanged(unsigned char note, bool primary_bank) {
-  unsigned long time = micros();
-
   if(note_velocity[note] == 0) {
     // Note is turning on
     if(primary_bank) {
@@ -75,9 +73,13 @@ void noteChanged(unsigned char note, bool primary_bank) {
       note_velocity[note] = microsToVelocity(time-half_on[note]);
       //half_on[note] = 0;
       noteOn(MIDI_CHAN, note, note_velocity[note]);
+      #ifdef SERIAL_DEBUG
       Serial.print(note, DEC);
       Serial.print(" is fully on  - ");
+      Serial.println(time-half_on[note], DEC);
+      Serial.print(" - ");
       Serial.println(note_velocity[note], DEC);
+      #endif
     }
   } else {
     // Note is turning off
@@ -85,9 +87,13 @@ void noteChanged(unsigned char note, bool primary_bank) {
       note_velocity[note] = microsToVelocity(time-half_off[note]);
       //half_off[note] = 0;
       noteOff(MIDI_CHAN, note, note_velocity[note]);
+      #ifdef SERIAL_DEBUG
       Serial.print(note, DEC);
       Serial.print(" is fully off - ");
+      Serial.println(time-half_on[note], DEC);
+      Serial.print(" - ");
       Serial.println(note_velocity[note], DEC);
+      #endif
       note_velocity[note] = 0;
     } else {
       //Serial.println(" is half off");
@@ -103,9 +109,11 @@ void bankChanged(int bank, unsigned char data) {
 
   primary_bank = (bank & PRIMARY_BANK_MASK) == PRIMARY_BANK_BITS;
 
+  #ifdef SERIAL_DEBUG
   Serial.print(bank);
   Serial.print("\t");
   Serial.println(data, HEX);
+  #endif
 
   for(pos = 0; pos < 8; pos++) {
     if(data & 0x01 == 0x01) {
@@ -130,18 +138,18 @@ void setup() {
   }
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(BLINKLED, OUTPUT);
+  #ifdef SERIAL_DEBUG
   Serial.begin(115200);
+  #endif
   // initialize the keyboard mux reader
   keyboard.init();
   keyboard.setBankChanged(bankChanged);
-  inverse_logic = keyboard.detectPullup();
 }
 
 // the loop function runs over and over again forever
 void loop() {
-
+  time = micros();
   keyboard.loop();
-  delayMicroseconds(10); // Minimum wait for MUX settling
-  // This wait time can be used for other calculations or communications
+  MidiUSB.flush();
 
 }
